eb85c9d92912ae3c453bdcce5caeb0ebc5ec5792
Mohamed Ahraf <mohamed7elprans@yahoo.com>
2022-05-20 08:18:48 +0200

    Create stack for parser

--- Compiler/Controllers/EditorController.cs
+++ Compiler/Controllers/EditorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Compiler.DataSturcutres;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Compiler.Controllers
---
+++ Compiler/DataSturcutres/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace Compiler.DataSturcutres
+{
+    public class Stack
+    {
+        private int Index;
+        private string[] Array;
+        public Stack(){
+            Array = new string[1000];
+            Index = 1;
+            Array[0] = "#";
+        }
+
+        public string Pop()
+        {
+            if (Index == 0) {
+                return "#";
+            }
+
+            return Array[--Index];
+        }
+
+        public void Push(string element)
+        {
+            if(Index + 1 >= Array.Length)
+            {
+                Debug.WriteLine("Out of index!");
+                return;
+            }
+            Array[Index++] = element;
+        }
+
+        public string GetLastElement()
+        {
+            if(Index == 0)
+            {
+                return "#";
+            }
+
+            return Array[Index - 1];
+        }
+    }
+}
